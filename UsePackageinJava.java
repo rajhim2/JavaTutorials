74078efff11175f610bfe091031d153e6bafda3a
Himanshu Raj <raj.himanshu2@gmail.com>
2018-01-18 09:06:53 +0530

    How to use Package in java for classes

---
+++ UsePackageinJava.java
@@ -0,0 +1,8 @@
+package world;
+public class UsePackageinJava{
+public static void main(String args[]){
+System.out.println("Hello World");
+}
+}
+//to compile use COMMAND--->>>>  javac -d . UsePackageinJava.java
+//to run write-------->>> java [packageName].[className]   ------>>>>> java  world.UsePackageinJava
