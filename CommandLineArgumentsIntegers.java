9a925d79ff959178a239c4a2fe87c169c0c1e319
Himanshu Raj <raj.himanshu2@gmail.com>
2018-01-18 09:01:35 +0530

    How to pass integer as arguments through main function

---
+++ CommandLineArgumentsIntegers.java
@@ -0,0 +1,8 @@
+public class CommandLineArgumentsIntegers{
+public static void main(String args[]){
+int s=0;
+for(int i=0;i<args.length;i++)
+s=s+Integer.parseInt(args[i]);
+System.out.println("Sum="+s);
+}
+}
