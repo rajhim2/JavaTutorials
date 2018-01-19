fa6bcb415d3b6089177c24a25a0322d487613e30
Himanshu Raj <raj.himanshu2@gmail.com>
2018-01-19 19:45:32 +0530

    Making Secondary class and it's package name is pack2

---
+++ ImportSecondaryClass.java
@@ -0,0 +1,17 @@
+package pack2;
+public class ImportSecondaryClass{
+private int rollNo;
+private String name;
+public void setName(String n){
+name=n;
+}
+public void setRoll(int t){
+rollNo=t;
+}
+public String getName(){
+return (name); 
+}
+public int getRoll(){
+return (rollNo);
+}
+}
