a99d8fda03128384f9cafa3f9f0dfe85840643f7
Himanshu Raj <raj.himanshu2@gmail.com>
2018-01-22 21:17:36 +0530

    A separate super class file is created which is inherited by SubClass

---
+++ Inheritance/InheritanceSuperClassPerson.java
@@ -0,0 +1,16 @@
+public class InheritanceSuperClassPerson{
+private int age;
+private String name;
+public void setAge(int a){
+age=a;
+}
+public int getAge(){
+return (age);
+}
+public void setName(String n){
+name=n;
+}
+public String getName(){
+return (name);
+}
+}
